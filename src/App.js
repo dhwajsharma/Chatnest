2785ea6d74ffb24132a199926fe4a791fce652f3
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-09-20 15:27:33 +0530

    Built Chat Header

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import styled from 'styled-components'
+import Chat from './components/Chat';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Sidebar />
             <Switch>
               <Route exact path="/" >
+                <Chat />
               </Route>
             </Switch>
           </AppBody>
