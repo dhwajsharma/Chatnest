5a5047cc47b480c0728d81e2cae337c95ea6b36b
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-09-20 12:52:45 +0530

    Implemented Redux for Sidebar

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,7 +1,10 @@
+import { useDispatch } from 'react-redux'
 import styled from 'styled-components'
+import { enterRoom } from '../features/appSlice'
 import { db } from "../firebase"
 
 const SidebarOption = ({ Icon, title, addChannelOption, id }) => {
+    const dispatch = useDispatch()
 
     const addChannel = () => {
         const channelName = prompt("Please enter the channel name");
@@ -14,7 +17,11 @@ const SidebarOption = ({ Icon, title, addChannelOption, id }) => {
     }
 
     const selectChannel = () => {
-
+        if (id) {
+            dispatch(enterRoom({
+                roomId: id,
+            }))
+        }
     }
 
     return (
