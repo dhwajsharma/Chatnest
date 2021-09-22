648a48b74b8e4db05b03436c84b5c0bea8536424
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-09-22 09:28:41 +0530

    Built Messaging section

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,18 @@
 import styled from "styled-components"
 
-const Message = () => {
+const Message = ({ message, timestamp, user, userImage }) => {
     return (
         <MessageContainer>
-
+            <img src={userImage} alt="" />
+            <MessageInfo>
+                <h4>
+                    {user}{" "}
+                    <span>
+                        {new Date(timestamp?.toDate()).toUTCString()}
+                    </span>
+                </h4>
+                <p>{message}</p>
+            </MessageInfo>
         </MessageContainer>
     )
 }
@@ -11,5 +20,23 @@ const Message = () => {
 export default Message
 
 const MessageContainer = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 20px;
 
+    >img{
+        height: 50px;
+        border-radius: 8px;
+    }
+`
+
+const MessageInfo = styled.div`
+    padding-left: 10px;
+
+    > h4 > span{
+        color: gray;
+        font-weight: 300;
+        margin-left: 4px;
+        font-size: 10px;
+    }
 `
