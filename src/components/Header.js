b960c351ae5a55237ab0d60fdebbbd8bf63ee292
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-01-07 19:27:44 +0530

    Added logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,99 +1,101 @@
-import { Avatar } from '@material-ui/core'
-import { AccessTime, HelpOutline, Search } from '@material-ui/icons'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import styled from 'styled-components'
-import { auth } from '../firebase'
+import { Avatar } from "@material-ui/core";
+import { AccessTime, HelpOutline, Search } from "@material-ui/icons";
+import { useAuthState } from "react-firebase-hooks/auth";
+import styled from "styled-components";
+import { auth } from "../firebase";
+import { useState } from "react";
 
 const Header = () => {
-    const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth);
+  const [logout, setLogout] = useState(null);
 
+  return (
+    <HeaderContainer>
+      <HeaderLeft>
+        <HeaderAvatar alt={user?.displayName} src={user?.photoURL} />
+        <LogoutButton onClick={() => auth.signOut()}>Logout</LogoutButton>
+        <AccessTime />
+      </HeaderLeft>
 
-    return (
-        <HeaderContainer>
-            <HeaderLeft>
-                <HeaderAvatar
-                    onClick={() => auth.signOut()}
-                    alt={user?.displayName}
-                    src={user?.photoURL}
-                />
-                <AccessTime />
-            </HeaderLeft>
+      <HeaderSearch>
+        <Search />
+        <input placeholder="Search" />
+      </HeaderSearch>
 
-            <HeaderSearch>
-                <Search />
-                <input placeholder="Search" />
-            </HeaderSearch>
+      <HeaderRight>
+        <HelpOutline />
+      </HeaderRight>
+    </HeaderContainer>
+  );
+};
 
-            <HeaderRight>
-                <HelpOutline />
-            </HeaderRight>
-
-        </HeaderContainer>
-    )
-}
-
-export default Header
+export default Header;
 
 const HeaderContainer = styled.div`
-    display: flex;
-    position: fixed;
-    width: 100%;
-    align-items: center;
-    justify-content: space-between;
-    padding: 10px 0;
-    background-color: var(--dark-color);
-    color: white;
-`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--dark-color);
+  color: white;
+`;
 
 const HeaderLeft = styled.div`
-    flex: 0.3;
-    display: flex;
-    align-items: center;
-    margin-left: 20px;
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
 
-    > .MuiSvgIcon-root{
-        margin-left: auto;
-        margin-right: 30px;
-        cursor: pointer;
-    }
-`
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+    cursor: pointer;
+  }
+`;
 
 const HeaderAvatar = styled(Avatar)`
-    cursor: pointer;
+  cursor: pointer;
 
-    :hover{
-        opacity: 0.8; 
-    }
-`
+  :hover {
+    opacity: 0.8;
+  }
+`;
 const HeaderSearch = styled.div`
-    flex: 0.4;
-    opacity: 1;
-    border-radius: 6px;
-    background-color: #3D3D3D;
-    text-align: center;
-    display: flex;
-    padding: 0 50px;    
-    color: gray;
-    border: 1px solid solid;
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #3d3d3d;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px solid solid;
 
-    >input {
-        background-color:transparent;
-        border: none;
-        text-align: center;
-        min-width: 30vw;
-        outline: none;
-        color: white;
-    }
-`
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: none;
+    color: white;
+  }
+`;
 
 const HeaderRight = styled.div`
-    flex: 0.3;
-    display: flex;
-    align-items: flex-end;
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
 
-    > .MuiSvgIcon-root{
-        margin-left: auto;
-        margin-right: 20px;
-        cursor: pointer;    
-    }
-`
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+    cursor: pointer;
+  }
+`;
+
+const LogoutButton = styled.div`
+  cursor: pointer;
+  margin-left: auto;
+`;
